82114d57e4a1dff58a328a2398ff7c12adb1b6d1
feliciano <felicianojrsousa@gmail.com>
2018-05-10 18:32:37 -0300

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+
+  output: {
+    path: path.resolve("public/assets"),
+    filename: "bundle.js",
+    publicPath: "assets"
+  },
+  devServer: {
+    inline: true,
+    contentBase: "./public",
+    port: 3000,
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      modules: path.join(__dirname, "/node_modules")
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: "babel-loader",
+            query: {
+              presets: ["react"]
+            }
+          }
+        ]
+      },
+      {
+        test: /(\.css|\.scss)$/,
+        loaders: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
