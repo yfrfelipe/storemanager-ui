2c3455846e902bc2ce4081ceba149a2f7005a165
feliciano <felicianojrsousa@gmail.com>
2018-05-13 21:57:16 -0300

    configure route!

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
+
 import NavBar from "./components/NavBar";
 import SideBarMenu from "./components/SideBarMenu";
 import MenuItem from "./components/MenuItem";
 import Card from "./components/Card";
+import DashBoard from "./scenes/dashboard";
+import Provider from "./scenes/provider";
+import Customer from "./scenes/customer";
+import Stock from "./scenes/stock";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        <div className="container-fluid">
-          <div className="row">
-            <SideBarMenu>
-              <MenuItem name="dash" />
-              <MenuItem name="dash3" />
-            </SideBarMenu>
-            <main
-              role="main"
-              className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4"
-            >
-              <div className="row">
-                <Card className="card-1"/>
-                <Card className="card-1"/>
-                <Card className="card-1"/>
-              </div>
-            </main>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <div className="container-fluid">
+            <div className="row">
+              <SideBarMenu>
+                <MenuItem size={20} icon="home" label="Dashboard" url="/dashboard"/>
+                <MenuItem size={20} icon="truck" label="Fornecedor" url="/provider"/>
+                <MenuItem size={20} icon="box" label="Estoque" url="/stock" />
+                <MenuItem size={20} icon="box" label="Clientes" url="/customer"/>
+              </SideBarMenu>
+              <main role="main"  className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+                <Route path="/dashboard" component={DashBoard} />
+                <Route path="/customer" component={Customer} />
+                <Route path="/provider" component={Provider} />
+                <Route path="/stock" component={Stock} />
+              </main>
+            </div>
           </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
 registerServiceWorker();
