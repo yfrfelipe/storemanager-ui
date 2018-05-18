b94b181047c73ec85fcb12b03bb3366b0d437ef2
feliciano <felicianojrsousa@gmail.com>
2018-05-18 16:33:29 -0300

    color navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 const NavBar = props => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <nav className="navbar navbar-expand-lg navbar-dark cas">
         <a className="navbar-brand">Y - Comercial</a>
       </nav>
     </div>
