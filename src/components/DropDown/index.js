5878a1fde8c4754390e924a4d60da22e59f50cf7
feliciano <felicianojrsousa@gmail.com>
2018-05-23 16:27:48 -0300

    fix dropdown

--- src/components/DropDown/index.js
+++ src/components/DropDown/index.js
@@ -7,13 +7,21 @@ class DropDown extends React.Component {
         this.state = {
             isToggleOn: false
         };
+        this.show = this.show.bind(this);
+        this.close = this.close.bind(this);
     }
 
-    showDropdown(e) {
+    show(e) {
         e.preventDefault();
-        this.setState(prevState => ({
-            isToggleOn: !prevState.isToggleOn
-        }));
+        this.setState({ isToggleOn: true }, () => {
+            document.addEventListener('click', this.close)
+        });
+    }
+
+    close() {
+        this.setState({ isToggleOn: false }, () => {
+            document.removeEventListener('click', this.close)
+        });
     }
 
     render() {
@@ -21,12 +29,12 @@ class DropDown extends React.Component {
         return (
             <div className="dropdown">
                 <button className="btn  dropdown-toggle btn-outline-light btn-sm"
-                 type="button" id="dropdownMenuButton"
-                 data-toggle="dropdown"  onClick={(e) => {this.showDropdown(e)}} 
-                 aria-haspopup="true" aria-expanded="false">
-                    Antônio 
+                    type="button" id="dropdownMenuButton"
+                    data-toggle="dropdown" onClick={(e) => { this.show(e) }}
+                    aria-haspopup="true" aria-expanded="false">
+                    Antônio
                 </button>
-                <div className={classDropdownMenu}  aria-labelledby="dropdownMenuButton">
+                <div className={classDropdownMenu} aria-labelledby="dropdownMenuButton">
                     <a className="dropdown-item" href="#">Action</a>
                     <a className="dropdown-item" href="#">Another</a>
                     <a className="dropdown-item" href="#">Something </a>
